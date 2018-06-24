60b8aad70ea0fae7d340b35a2d01419fac5b1830
NitramLegov <martin_vogel@live.com>
2018-06-24 14:50:20 +0200

    trying different versions of cncjs installation

--- install_cncjs.sh
+++ install_cncjs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Now we will install cncjs'
-sudo npm install  -g cncjs@latest --unsafe-perm --quiet
+sudo npm install -g cncjs@latest --quiet
 sudo cp cncjs.service /etc/systemd/system/cncjs.service
 sudo systemctl daemon-reload
 sudo systemctl enable cncjs.service
