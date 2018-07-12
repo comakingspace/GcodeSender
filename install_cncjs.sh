44b616f5b4e86643462cf614b1fca818f29dca27
NitramLegov <martin_vogel@live.com>
2018-07-12 21:42:26 +0200

    Adding cncjs settings - to be tried

--- install_cncjs.sh
+++ install_cncjs.sh
@@ -5,6 +5,7 @@ echo 'Now we will install cncjs'
 #This works:
 sudo npm install -g cncjs@latest --unsafe-perm --quiet # Seems to work
 sudo cp cncjs.service /etc/systemd/system/cncjs.service
+cp cncrc_settings.json ~/.cncrc
 sudo systemctl daemon-reload
 sudo systemctl enable cncjs.service
 sudo systemctl start cncjs
