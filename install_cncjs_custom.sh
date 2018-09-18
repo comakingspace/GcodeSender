04756c4ee682cea48cd2242e6090e8fca26c92b7
NitramLegov <martin_vogel@live.com>
2018-09-18 22:05:30 +0200

    ..

--- install_cncjs_custom.sh
+++ install_cncjs_custom.sh
@@ -9,13 +9,20 @@ sudo cp dphys-swapfile_3GB /etc/dphys-swapfile
 sudo systemctl stop dphys-swapfile.service
 sudo systemctl start dphys-swapfile.service
 
-#now we can install cncjs
+echo 'Now we can clone and build out CNCjs fork'
 cd ..
 git clone https://github.com/comakingspace/cncjs/
 cd cncjs
 npm install -g npm@5.6.0
 npm install --build-from-source
 cd ../GcodeSender
+
+echo 'Revert the swapfile changes...'
+sudo cp dphys-swapfile_100MB /etc/dphys-swapfile
+sudo systemctl stop dphys-swapfile.service
+sudo systemctl start dphys-swapfile.service
+
+echo 'And finally start the service.'
 sudo cp cncjs_custom.service /etc/systemd/system/cncjs_custom.service
 cp cncrc_settings.json ~/.cncrc
 sudo systemctl daemon-reload
