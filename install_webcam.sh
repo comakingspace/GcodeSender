db58432b5c827fab53e30d1e9b6d5622037d87be
NitramLegov <martin_vogel@live.com>
2018-09-18 22:49:03 +0200

    .

--- install_webcam.sh
+++ install_webcam.sh
@@ -4,6 +4,7 @@ git clone https://github.com/jacksonliam/mjpg-streamer
 cd mjpg-streamer/mjpg-streamer-experimental
 make
 sudo make install
+cd ../..
 sudo cp mjpeg_streamer.service /etc/systemd/system/mjpeg_streamer.service
 sudo systemctl daemon-reload
 sudo systemctl enable mjpeg_streamer.service
