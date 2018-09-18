54ce814cf9b83fc4ba9d6e90189bdb9550a2f1c3
NitramLegov <martin_vogel@live.com>
2018-09-18 21:00:49 +0200

    ..

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 echo 'please do not run this as root'
 sudo ./install_systemsettings.sh
 sudo ./install_nicetohavesoftware.sh
-screen -R CNCInstall
-sudo ./install_node.sh
-#./install_laserweb4.sh
-#./install_cncjs.sh
-./install_cncjs_custom.sh
-#./install_grblweb.sh
-./install_webcam.sh
+
+screen -dmS CNCInstall bash -c 'sudo ./install_node.sh; ./install_cncjs_custom.sh; ./install_webcam.sh; exec bash'
+# sudo ./install_node.sh
+# #./install_laserweb4.sh
+# #./install_cncjs.sh
+# ./install_cncjs_custom.sh
+# #./install_grblweb.sh
+# ./install_webcam.sh
 echo 'All done, it would be a great idea to reboot now.'
